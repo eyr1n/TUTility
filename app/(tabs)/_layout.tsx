5922ac3e1b912462f37c14ecbe490ba3d5d6b122
Rin Iwai <rin@eyrin.jp>
2025-05-25 13:32:58 +0900

    react compiler動く？

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
-import { BottomTabBar } from '@/components/BottomTabBar';
 import { MaterialIcons } from '@expo/vector-icons';
+import { CommonActions } from '@react-navigation/native';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, BottomNavigation } from 'react-native-paper';
 
 export default function TabLayout() {
   return (
@@ -14,7 +14,46 @@ export default function TabLayout() {
           </Appbar.Header>
         ),
       }}
-      tabBar={BottomTabBar}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) =>
+            descriptors[route.key].options.tabBarIcon?.({
+              focused,
+              color,
+              size: 24,
+            }) || null
+          }
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              typeof options.tabBarLabel === 'string'
+                ? options.tabBarLabel
+                : typeof options.title === 'string'
+                  ? options.title
+                  : route.name;
+
+            return label;
+          }}
+        />
+      )}
     >
       <Tabs.Screen
         name="index"
